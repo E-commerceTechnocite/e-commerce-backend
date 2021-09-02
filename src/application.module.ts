64b7d7f54c711e7e0378b67fdae72bd3e06d073b
Paul <paul.ned@outlook.com>
2021-09-02 14:45:09 +0200

    Added config module and for env variables

--- src/application.module.ts
+++ src/application.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ApplicationConfigModule } from '@app/application-config.module';
 
 @Module({
-  imports: [],
+  imports: [ApplicationConfigModule],
   controllers: [],
   providers: [],
 })
