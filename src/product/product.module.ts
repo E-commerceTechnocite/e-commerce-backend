cad0688111a4ce3c96b6a8408bd2296178eef116
Paul <paul.ned@outlook.com>
2021-09-02 17:07:59 +0200

    Removed product repository form module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,11 +3,10 @@ import { ProductController } from '@app/product/product.controller';
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '@app/product/product.entity';
-import { ProductRepository } from '@app/product/product.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
-  providers: [ProductService, ProductRepository],
+  providers: [ProductService],
 })
 export class ProductModule {}
