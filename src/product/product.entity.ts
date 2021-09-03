11269f5be89da16651e0967d009d25e7298e2480
Paul <paul.ned@outlook.com>
2021-09-03 08:58:09 +0200

    Added product entity properties

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,7 +1,15 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column()
+  title: string;
+  @Column()
+  reference: string;
+  @Column({ type: 'text' })
+  description: string;
+  @Column({ type: 'float' })
+  price: number;
 }
