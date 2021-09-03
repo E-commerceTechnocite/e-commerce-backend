88a2df893043242c17cd0696080642a804cd74fd
Paul <paul.ned@outlook.com>
2021-09-03 09:07:59 +0200

    Crud service interface update

--- src/interfaces/crud-service.interface.ts
+++ src/interfaces/crud-service.interface.ts
@@ -1,8 +1,13 @@
+import { DeleteResult, InsertResult, UpdateResult } from 'typeorm';
+
 export interface CrudServiceInterface<T> {
-  find(id: string | number): T;
-  findAll(): T[];
-  create(entity: T): void | T | Promise<void> | Promise<T>;
-  update(id: string | number, entity: T): void | T | Promise<void> | Promise<T>;
-  deleteFromId(id: string | number): void | Promise<void>;
-  delete(entity: T): void | Promise<void>;
+  find(id: string | number): T | Promise<T>;
+  findAll(): T[] | Promise<T[]>;
+  create(entity: T): Promise<void> | Promise<T> | Promise<InsertResult>;
+  update(
+    id: string | number,
+    entity: T,
+  ): Promise<void> | Promise<T> | Promise<UpdateResult>;
+  deleteFromId(id: string | number): Promise<void> | Promise<DeleteResult>;
+  delete(entity: T): Promise<void> | Promise<DeleteResult>;
 }
