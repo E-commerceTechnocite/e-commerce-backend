6918ac7494bb1d3580d16a6e9838ad85010744e1
Paul <paul.ned@outlook.com>
2021-09-03 09:51:04 +0200

    Added OpenApi documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from '@app/application.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce endpoints documentation')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 
