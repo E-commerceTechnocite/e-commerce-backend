b418fb98c90686010e47b129b20b30ed459b0b7d
Paul <paul.ned@outlook.com>
2021-09-06 11:04:08 +0200

    product controller and service tests

--- src/product/controllers/product/product.controller.spec.ts
+++ src/product/controllers/product/product.controller.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from '@app/product/controllers/product/product.controller';
+import {
+  ProductService,
+  ProductServiceInterface,
+} from '@app/product/services/product/product.service';
+import { Product } from '@app/product/entities/product.entity';
+import { DeleteResult, InsertResult, UpdateResult } from 'typeorm';
 
 describe('ProductController', () => {
   let controller: ProductController;
+  // Mock implementation of the product service
+  const service: ProductServiceInterface = {
+    create(
+      entity: Product,
+    ): Promise<void> | Promise<Product> | Promise<InsertResult> {
+      return Promise.resolve(undefined);
+    },
+    delete(entity: Product): Promise<void> | Promise<DeleteResult> {
+      return Promise.resolve(undefined);
+    },
+    find(id: string | number): Promise<Product> | Product {
+      return undefined;
+    },
+    findAll(): Product[] | Promise<Product[]> {
+      return undefined;
+    },
+    update(
+      id: string | number,
+      entity: Product,
+    ): Promise<void> | Promise<Product> | Promise<UpdateResult> {
+      return Promise.resolve(undefined);
+    },
+    deleteFromId(id: string | number): Promise<void> | Promise<DeleteResult> {
+      return Promise.resolve(undefined);
+    },
+  };
+  const item: Product = {
+    price: 39.99,
+    reference: '1234',
+    title: 'title',
+    id: '1',
+    description: 'description',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-    }).compile();
+      providers: [ProductService],
+    })
+      .overrideProvider(ProductService)
+      .useValue(service)
+      .compile();
 
     controller = module.get<ProductController>(ProductController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findall', () => {
+    it('should return an array of products', async () => {
+      const product = item;
+      jest.spyOn(service, 'findAll').mockResolvedValueOnce([product]);
+      expect(await controller.findAll()).toEqual([product]);
+    });
   });
 });
--- src/product/services/product/product.service.spec.ts
+++ src/product/services/product/product.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from '@app/product/services/product/product.service';
+import { Product } from '@app/product/entities/product.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ProductService', () => {
   let service: ProductService;
+  // Mock implementation for the product repository
+  let repository: Repository<Product>;
+  const item: Product = {
+    title: 'title',
+    reference: '1234',
+    price: 39.99,
+    description: 'description',
+    id: '1',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        { provide: getRepositoryToken(Product), useClass: Repository },
+      ],
     }).compile();
-
     service = module.get<ProductService>(ProductService);
+
+    repository = module.get<Repository<Product>>(getRepositoryToken(Product));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findall', () => {
+    it('should return a list of products', async () => {
+      const products: Product[] = [item];
+      jest.spyOn(repository, 'find').mockResolvedValueOnce(products);
+      expect(await service.findAll()).toEqual(products);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a product', async () => {
+      const product: Product = item;
+      jest.spyOn(repository, 'findOne').mockResolvedValueOnce(product);
+      expect(await service.find('1')).toEqual(product);
+    });
   });
 });
