d6c4e8d60ea6cece25f1984bbf6328bdd2aea18e
Paul <paul.ned@outlook.com>
2021-09-08 10:37:26 +0200

    Async config for typeorm

--- src/configuration/application-configuration.module.ts
+++ src/configuration/application-configuration.module.ts
@@ -1,18 +1,16 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigurationService } from '@app/configuration/type-orm/type-orm-configuration.service';
 
+@Global()
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env['DATABASE_HOST'],
-      port: +process.env['DATABASE_PORT'],
-      username: process.env['DATABASE_USER'],
-      password: process.env['DATABASE_PASSWORD'],
-      database: process.env['DATABASE_NAME'],
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigurationService,
+      inject: [ConfigService],
     }),
   ],
   exports: [ConfigModule, TypeOrmModule],
