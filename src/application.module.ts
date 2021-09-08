2d088333ff016238f7d5d4ea369c598761261f48
Paul <paul.ned@outlook.com>
2021-09-08 16:16:33 +0200

    Added morgan for logging

--- src/application.module.ts
+++ src/application.module.ts
@@ -1,8 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ProductModule } from '@app/product/product.module';
 import { ApplicationConfigurationModule } from '@app/configuration/application-configuration.module';
+import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [ApplicationConfigurationModule, ProductModule],
 })
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule {
+  constructor(private readonly config: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(morgan(this.config.get<string>('MORGAN_FORMAT') ?? 'dev'))
+      .forRoutes('*');
+  }
+}
