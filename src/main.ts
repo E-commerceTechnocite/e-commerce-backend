880234de332a52cff963fdcb4a5de9c0e45107b4
Paul <paul.ned@outlook.com>
2021-09-09 15:39:45 +0200

    Added validation dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from '@app/application.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
 
   app.enableVersioning({ type: VersioningType.URI });
-
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('E-commerce endpoints documentation')
     .build();
