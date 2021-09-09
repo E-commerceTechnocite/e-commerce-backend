55bac21ad67e653acda7dcb16123fab0948d40c2
Paul <paul.ned@outlook.com>
2021-09-09 16:11:54 +0200

    Added validation for product and category

--- src/product/dto/product/product.dto.ts
+++ src/product/dto/product/product.dto.ts
@@ -1,18 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsUUID,
+  Length,
+} from 'class-validator';
 
 export class ProductDto {
   @ApiProperty({ required: false })
+  @Length(2, 255)
+  @IsNotEmpty()
   title: string;
 
   @ApiProperty({ required: false })
+  @Length(2, 255)
+  @IsNotEmpty()
   reference: string;
 
   @ApiProperty({ required: false })
+  @Length(10)
+  @IsNotEmpty()
   description: string;
 
   @ApiProperty({ required: false })
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  @IsNotEmpty()
   price: number;
 
   @ApiProperty({ required: false })
+  @IsUUID()
+  @IsNotEmpty()
   categoryId: string;
 }
