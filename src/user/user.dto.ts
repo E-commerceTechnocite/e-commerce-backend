365a3a8a0e96305a76b2463aaafc23c91f646c5c
Paul <paul.ned@outlook.com>
2021-09-13 16:15:51 +0200

    Removed username from swagger doc

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -3,8 +3,9 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UserDto {
   @ApiProperty({ required: false })
   email?: string;
-  @ApiProperty({ required: false })
+
   username?: string;
+
   @ApiProperty({ required: false })
   password?: string;
 }
