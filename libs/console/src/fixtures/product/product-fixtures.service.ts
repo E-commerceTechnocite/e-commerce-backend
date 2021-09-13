b02b761d13883a3f4a5ec93114168ffdc687fa3b
Paul <paul.ned@outlook.com>
2021-09-13 10:32:55 +0200

    Added product and category fixtures

--- libs/console/src/fixtures/product/product-fixtures.service.ts
+++ libs/console/src/fixtures/product/product-fixtures.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from '@app/product/entities/product.entity';
 import { Repository } from 'typeorm';
 import { ProductCategory } from '@app/product/entities/product-category.entity';
+import * as faker from 'faker';
 
 @Injectable()
 export class ProductFixturesService {
@@ -14,11 +15,26 @@ export class ProductFixturesService {
   ) {}
 
   async load() {
-    //  TODO
+    const category: ProductCategory = {
+      label: faker.commerce.productAdjective(),
+    };
+    const savedCategory: ProductCategory = await this.categoryRepo.save(
+      category,
+    );
+    for (let i = 0; i < 10; i++) {
+      const product: Product = {
+        reference: faker.random.alphaNumeric(10),
+        title: faker.commerce.product(),
+        price: +faker.commerce.price(1, 100, 2),
+        description: faker.random.words(50),
+        category: savedCategory,
+      };
+      await this.productRepo.save(product);
+    }
   }
 
   async clean() {
-    await this.categoryRepo.clear();
     await this.productRepo.clear();
+    await this.categoryRepo.clear();
   }
 }
