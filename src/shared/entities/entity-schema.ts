8add6e578d13c311bc4e9fa154022da98914904b
Paul <paul.ned@outlook.com>
2021-09-14 12:00:55 +0200

    Added entity base schema

---
+++ src/shared/entities/entity-schema.ts
@@ -0,0 +1,15 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export abstract class EntitySchema {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'datetime' })
+  createdAt: Date;
+
+  @Column({ type: 'datetime' })
+  updatedAt: Date;
+
+  @Column({ type: 'datetime' })
+  deletedAt: Date;
+}
