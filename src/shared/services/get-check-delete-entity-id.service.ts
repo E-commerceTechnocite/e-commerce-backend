f0730ae3a70d9015076e8ea23148d7b597511fbe
Sacha <sahiouni.sacha@outlook.com>
2021-09-17 16:06:30 +0200

    product service facto

--- src/shared/services/get-check-delete-entity-id.service.ts
+++ src/shared/services/get-check-delete-entity-id.service.ts
@@ -1,16 +1,14 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class GetCheckDeleteEntityIdService {
-
-
-    async getEntity<T>(repo : Repository<T>, dto : any, id : string):Promise<T>{
-        const entity = await repo.findOne(dto[id]);
-        if (!entity) {
-          throw new BadRequestException(`Not found at id ${dto[id]}`);
-        }
-        delete dto[id];
-        return entity;
-      }
+  async getEntity<T>(repo: Repository<T>, dto: any, id: string): Promise<T> {
+    const entity = await repo.findOne(dto[id]);
+    if (!entity) {
+      throw new BadRequestException(`Not found at id ${dto[id]}`);
+    }
+    delete dto[id];
+    return entity;
+  }
 }
