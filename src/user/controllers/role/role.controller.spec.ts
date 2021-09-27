da9e5a4879fa6306cc579b6b096189885a70b238
Sacha <sahiouni.sacha@outlook.com>
2021-09-27 14:54:36 +0200

    tests taxrule role file

--- src/user/controllers/role/role.controller.spec.ts
+++ src/user/controllers/role/role.controller.spec.ts
@@ -1,12 +1,16 @@
+import { RoleService } from '@app/user/services/role/role.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { mock } from 'jest-mock-extended';
 import { RoleController } from './role.controller';
 
 describe('RoleController', () => {
   let controller: RoleController;
+  const roleService = mock<RoleService>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RoleController],
+      providers: [{ provide: RoleService, useValue: roleService }],
     }).compile();
 
     controller = module.get<RoleController>(RoleController);
--- src/user/services/role/role.service.spec.ts
+++ src/user/services/role/role.service.spec.ts
@@ -1,12 +1,23 @@
+import { Role } from '@app/user/entities/role.entity';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { mock } from 'jest-mock-extended';
+import { Repository } from 'typeorm';
 import { RoleService } from './role.service';
 
 describe('RoleService', () => {
   let service: RoleService;
+  const roleRepository = mock<Repository<Role>>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoleService],
+      providers: [
+        RoleService,
+        {
+          provide: getRepositoryToken(Role),
+          useValue: roleRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<RoleService>(RoleService);
