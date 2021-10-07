afc346cd629b1ab8e0f4dd759e1919605152eb8a
Brahim <brahimaz@hotmail.com>
2021-10-07 13:13:15 +0200

    working on public controller plus mock ProductService

--- src/product/controllers-public/product/product.controller.spec.ts
+++ src/product/controllers-public/product/product.controller.spec.ts
@@ -1,12 +1,15 @@
+import { ProductService } from '@app/product/services/product/product.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { mock } from 'jest-mock-extended';
 import { ProductController } from './product.controller';
 
 describe('ProductController', () => {
   let controller: ProductController;
-
+  const productServiceMock = mock<ProductService>();
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
+      providers: [{ provide: ProductService, useValue: productServiceMock }],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
