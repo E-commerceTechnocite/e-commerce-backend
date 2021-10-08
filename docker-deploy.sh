eed8104557bf7dc096efa4503e3665e69549aa5c
Paul <paul.ned@outlook.com>
2021-10-08 19:46:30 +0200

    docker-deploy.sh update

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 date=$(date +%s)
-tag="$DOCKERHUB_USERNAME/e-commerce-backend:$date"
-latest="$DOCKERHUB_USERNAME/e-commerce-backend:latest"
+image_name="$DOCKERHUB_USERNAME/e-commerce-backend"
+tag="$image_name:$date"
+latest="$image_name:latest"
 
-docker build . --file Dockerfile -t "$tag" -t "$latest"
+docker build . --file Dockerfile -t "$tag"
+docker tag "$tag" "$latest"
 
 docker push "$tag"
 docker push "$latest"
