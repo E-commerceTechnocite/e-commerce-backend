622f81ec87e0deef0d72b3e87c3a01a81931e3f2
Paul <paul.ned@outlook.com>
2021-10-08 19:32:55 +0200

    docker-deploy string formatting

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 date=$(date +%s)
-tag="$DOCKERHUB_USERNAME"/e-commerce-backend:"$date"
-latest="$DOCKERHUB_USERNAME"/e-commerce-backend:latest
+tag="$DOCKERHUB_USERNAME/e-commerce-backend:$date"
+latest="$DOCKERHUB_USERNAME/e-commerce-backend:latest"
 
 docker build . --file Dockerfile -t "$tag" -t "$latest"
 
