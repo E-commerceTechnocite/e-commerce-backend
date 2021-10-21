bda74793106f5253ea53c267a26affc29fcd1c23
Paul <paul.ned@outlook.com>
2021-10-21 09:08:59 +0200

    Admin login check and permissions

--- src/auth/admin/o-auth/o-auth.controller.ts
+++ src/auth/admin/o-auth/o-auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { OAuthService } from '@app/auth/admin/o-auth/o-auth.service';
 import { UserLogDto } from '@app/user/user-log.dto';
 import { RefreshTokenDto } from '../dto/refresh-token.dto';
+import { ApiAdminAuth } from '@app/shared/swagger';
 
 @ApiTags('Security')
 @Controller({ path: 'o-auth', version: '1' })
@@ -22,5 +23,22 @@ export class OAuthController {
   @Post('logout')
   async logout(@Body() { refresh_token }: RefreshTokenDto) {
     return await this.oAuthService.logout(refresh_token);
+  }
+
+  @ApiAdminAuth()
+  @Post('check')
+  async check() {
+    return await this.oAuthService.check();
+  }
+
+  @ApiAdminAuth()
+  @ApiResponse({
+    description: "returns the authenticated user's permissions",
+    type: 'string',
+    isArray: true,
+  })
+  @Get('permissions')
+  async getPermissions() {
+    return await this.oAuthService.getPermissions();
   }
 }
