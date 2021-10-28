2441015a51559b34eef46e49e15ef08c70e99cfb
Paul <paul.ned@outlook.com>
2021-10-28 16:49:20 +0200

    Added mocks in tests

--- src/customer/services/customer/customer.service.spec.ts
+++ src/customer/services/customer/customer.service.spec.ts
@@ -4,16 +4,19 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { mock } from 'jest-mock-extended';
 import { Repository } from 'typeorm';
 import { CustomerService } from './customer.service';
+import { MysqlSearchEngineService } from '@app/shared/services/mysql-search-engine.service';
 
 describe('CustomerService', () => {
   let service: CustomerService;
   const customer = mock<Repository<Customer>>();
+  const searchEngineService = mock<MysqlSearchEngineService>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CustomerService,
         { provide: getRepositoryToken(Customer), useValue: customer },
+        { provide: MysqlSearchEngineService, useValue: searchEngineService },
       ],
     }).compile();
 
