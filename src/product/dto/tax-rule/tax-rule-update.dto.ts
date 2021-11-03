cefdfaa1e92cdf1edb47c69172271e466d8fda0a
Sacha <sahiouni.sacha@outlook.com>
2021-11-03 16:25:40 +0100

    tax delete

--- src/product/dto/tax-rule/tax-rule-update.dto.ts
+++ src/product/dto/tax-rule/tax-rule-update.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsUUID, Length } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsPositive, Length } from 'class-validator';
 
 export class TaxRuleUpdateDto {
   @ApiProperty({ required: false })
-  @IsUUID()
+  @IsNumber()
+  @IsPositive()
   @IsNotEmpty()
-  taxId?: string;
+  tax?: number;
 
   @ApiProperty({ required: false })
   @Length(2, 10)
