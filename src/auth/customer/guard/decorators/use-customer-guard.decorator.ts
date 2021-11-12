b18f19a871e42cfcdc7f172a5bee8322eeb6c9de
Paul <paul.ned@outlook.com>
2021-11-12 12:52:04 +0100

    :pencil2: Customer guard decorator typo

--- src/auth/customer/guard/decorators/use-customer-guard.decorator.ts
+++ src/auth/customer/guard/decorators/use-customer-guard.decorator.ts
@@ -2,5 +2,5 @@ import { applyDecorators, UseGuards } from '@nestjs/common';
 import { ApiCustomerAuth } from '@app/shared/swagger';
 import { JwtAuthGuard } from '@app/auth/customer/guard/customer-jwt-auth.guard';
 
-export const UseCustomerGuardDecorator = (): MethodDecorator & ClassDecorator =>
+export const UseCustomerGuard = (): MethodDecorator & ClassDecorator =>
   applyDecorators(ApiCustomerAuth(), UseGuards(JwtAuthGuard));
